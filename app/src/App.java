4904ac7d993ea408648ae990b419e6c49007fc6e
Jon Morato <ikjmorato26@lhusurbil.eus>
2026-09-25 10:22:25 +0200

    quitando texto

--- app/src/App.java
+++ app/src/App.java
@@ -1,10 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
+
         int i = 0;
-=======
-        int j = 0;
->>>>>>> feature/app-idazten
+
     }
 }
